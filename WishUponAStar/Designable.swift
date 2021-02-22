bcf02f78d8039fb58f38c5eb1d6c6e2b5b478d30
nullworkbench <nullworkbench@gmail.com>
2021-02-22 18:45:58 +0900

    コンパス実装

---
+++ WishUponAStar/Designable.swift
@@ -0,0 +1,65 @@
+//
+//  Designable.swift
+//  WishUponAStar
+//
+//  Created by nullworkbench on 2021/02/22.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+}
+
+extension UIView {
+    
+    @IBInspectable
+    var isCircle: Bool {
+        get {
+            return true
+        }
+        set {
+            if newValue {
+                layer.cornerRadius =  self.frame.size.width / 2.2
+            }
+        }
+    }
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+        var borderWidth: CGFloat {
+            get {
+                return layer.borderWidth
+            }
+            set {
+                layer.borderWidth = newValue
+            }
+        }
+
+        @IBInspectable
+        var borderColor: UIColor? {
+            get {
+                if let color = layer.borderColor {
+                    return UIColor(cgColor: color)
+                }
+                return nil
+            }
+            set {
+                if let color = newValue {
+                    layer.borderColor = color.cgColor
+                } else {
+                    layer.borderColor = nil
+                }
+            }
+        }
+    
+}
--- WishUponAStar/ViewController.swift
+++ WishUponAStar/ViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    // ロケーションマネージャー定義
+    let locationManager = CLLocationManager()
+    
+    // コンパスのView
+    @IBOutlet var compassView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print(compassView.frame.size.width / 2.1)
+        
+        // ロケーションマネージャーのdelegate指定
+        locationManager.delegate = self
+        // 方角の更新間隔
+        locationManager.headingFilter = 0.1
+        // 方角の取得開始
+        locationManager.startUpdatingHeading()
+    }
+    
+    // 方角が変わると呼び出される
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        compassView.transform = CGAffineTransform(rotationAngle: CGFloat(-newHeading.magneticHeading) * CGFloat.pi / 180)
     }
 
 
