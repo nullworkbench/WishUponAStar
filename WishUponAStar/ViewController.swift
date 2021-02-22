164b24c5b2faeaa29c16b07608eb70ed5c4d5e0b
nullworkbench <nullworkbench@gmail.com>
2021-02-22 16:02:33 +0900

    Initial Commit

---
+++ WishUponAStar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WishUponAStar
+//
+//  Created by nullworkbench on 2021/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
