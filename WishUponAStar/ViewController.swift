08186d3fa75d3af06ca529bf8ec566e055f5bfeb
nullworkbench <nullworkbench@gmail.com>
2021-02-23 23:29:02 +0900

    starImageとstarLabelを分離し、Starがきちんとした位置に表示されるように

--- WishUponAStar/ViewController.swift
+++ WishUponAStar/ViewController.swift
@@ -57,14 +57,55 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                     print("new: \(data)")
                     
                     // PostViewをコンパスに追加
-                    let posX: CGFloat = self.calcXfromDirection((data["direction"] as? CGFloat)!)
-                    let posY: CGFloat = self.calcYfromDirection((data["direction"] as? CGFloat)!)
-                    let width: CGFloat = self.compassView.bounds.size.width * 0.5 // compassView幅の50%
-                    let height: CGFloat = width * 0.2 // width:height = 10:2
-                    let postView: PostView = PostView(frame: CGRect(x: posX - (width * 0.1), y: posY - (height / 2), width: width, height: height)) // サイズと位置を指定
+//                    let posX: CGFloat = self.calcXfromDirection((data["direction"] as? CGFloat)!)
+//                    let posY: CGFloat = self.calcYfromDirection((data["direction"] as? CGFloat)!)
+//                    let width: CGFloat = self.compassView.bounds.size.width * 0.5 // compassView幅の50%
+//                    let height: CGFloat = width * 0.2 // width:height = 10:2
+//                    let postView: PostView = PostView(frame: CGRect(x: posX - (width * 0.1), y: posY - (height / 2), width: width, height: height)) // サイズと位置を指定
+//
+//                    postView.wishLabel.text = data["wish"] as? String // 願いごとを代入
+//                    self.compassView.addSubview(postView) // 親Viewに追加
                     
-                    postView.wishLabel.text = data["wish"] as? String // 願いごとを代入
-                    self.compassView.addSubview(postView) // 親Viewに追加
+                    // starViewを作成
+                    let starPosX: CGFloat = self.calcXfromDirection((data["direction"] as? CGFloat)!)
+                    let starPosY: CGFloat = self.calcYfromDirection((data["direction"] as? CGFloat)!)
+                    let starSize: CGFloat = self.compassView.bounds.size.width * 0.1 // compassView幅の50%
+                    let starImageView = UIImageView(frame: CGRect(x: starPosX - (starSize / 2), y: starPosY - (starSize / 2), width: starSize, height: starSize))
+                    // starImageViewに画像を設定
+                    starImageView.image = UIImage(named: "star")
+                    // starImageViewをcompassViewへ追加
+                    self.compassView.addSubview(starImageView)
+                    
+                    // starLabelViewを作成
+                    let starLabelViewPosX = starPosX + starSize / 2
+                    let starLabelViewPosY = starPosY - starSize / 5
+                    let starLabelViewSize = self.compassView.bounds.size.width * 0.3
+                    let starLabelView = UIView(frame: CGRect(x: starLabelViewPosX, y: starLabelViewPosY, width: starLabelViewSize, height: starLabelViewSize * 0.3))
+//                    let starLabelView = UIView()
+                    starLabelView.backgroundColor = UIColor(white: 1, alpha: 0.5) // 背景色
+                    // starLabel作成
+                    let starLabel = UILabel(frame: CGRect(x: 10, y: 0, width: starLabelViewSize - 20, height: starLabelViewSize * 0.3))
+//                    let starLabel = UILabel()
+                    starLabel.text = data["wish"] as? String // 願いごとを代入
+                    starLabel.font = UIFont(name: "Hiragino Maru Gothic ProN", size: 9) // フォント設定
+                    starLabel.textColor = UIColor.black // 文字色
+                    starLabel.backgroundColor = UIColor.clear // 背景色
+                    starLabelView.addSubview(starLabel) // starLabelViewに追加
+                    // AutoLayout
+//                    starLabelView.frame = self.view.frame
+//                    starLabel.frame = self.view.frame
+//                    NSLayoutConstraint.activate([
+//                        starLabelView.widthAnchor.constraint(greaterThanOrEqualToConstant: 50),
+//                        starLabelView.heightAnchor.constraint(equalToConstant: starLabelView.frame.size.width * 0.3),
+//                        starLabel.widthAnchor.constraint(greaterThanOrEqualToConstant: 50),
+//    //                    starLabel.leadingAnchor.constraint(equalTo: starLabelView.leftAnchor, constant: 10),
+//    //                    starLabel.trailingAnchor.constraint(equalTo: starLabelView.rightAnchor, constant: 10),
+//    //                    starLabel.topAnchor.constraint(equalTo: starLabelView.topAnchor, constant: 10),
+//    //                    starLabel.bottomAnchor.constraint(equalTo: starLabelView.bottomAnchor, constant: 10),
+//                    ])
+                    
+                    // starLabelViewをcompassViewに追加
+                    self.compassView.addSubview(starLabelView)
                 }
             }
         }
@@ -77,6 +118,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
         // compassViewを回転
         compassView.transform = CGAffineTransform(rotationAngle: CGFloat(-newHeading.magneticHeading) * CGFloat.pi / 180)
+        // compassViewの子Viewの水平を維持
+        for view in compassView.subviews {
+            view.transform = CGAffineTransform(rotationAngle: -CGFloat(-newHeading.magneticHeading) * CGFloat.pi / 180)
+        }
         // 現在の方角を更新
         currentDirection = Float(newHeading.magneticHeading)
     }
